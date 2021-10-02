877f00edfa5e64f2175799b8b49f984f3bb928cc
arudovwen <successahon@gmail.com>
2021-10-02 11:51:33 +0100

    update

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -12,54 +12,65 @@ function AboutUs({ sidebar, setSidebar }) {
     <div className={`about-us ${sidebar ? "open" : ""}`}>
       <Header sidebar={sidebar} setSidebar={setSidebar} page={"about-page"} />
       <main className="container">
-        <h1 className="mb-4">Overview</h1>
+        <h1 className="mb-3">Overview</h1>
         <p>
           Thematic Resources Ltd is a Nigerian based oil and gas ("O&G") company
           with focus in acquiring assets for Exploration and Production. In the
           main time, we provide a wide range of upstream geophysical data
           services, mid & down stream products and some interest in renewable
-          energy. TR undertake to maximise the Local Content of any project it
-          executes, while maintaining the local and international quality and
-          safety standards..
+          energy.
+        </p>
+        <p>
+          TR undertake to maximise the Local Content of any project it executes,
+          while maintaining the local and international quality and safety
+          standards..
         </p>
 
         <section className="who-we-are-wrapper mt-5">
           <h2 className="line-header">Who We Are</h2>
           <div className="wrapper row mb-5">
             <div className="first col-sm-6">
-              <img src={FirstIcon} alt="" /> <br />
-              <h5>Our Purpose</h5>
-              <p>
-                Our purpose is to produce and deliver energy, water and food to
-                the world in a sustainable matter
-              </p>
+              <div>
+                <img src={FirstIcon} alt="" /> <br />
+                <h5>Our Purpose</h5>
+                <p>
+                  Our purpose is to produce and deliver energy, water and food
+                  to the world in a sustainable matter
+                </p>
+              </div>
             </div>
             <div className="first col-sm-6">
-              <img src={SecondIcon} alt="" /> <br />
-              <h5>Our vision</h5>
-              <p>
-                To become a leading independent Company combining best
-                practices, exceptional people and technical expertise
-              </p>
+              <div>
+                <img src={SecondIcon} alt="" /> <br />
+                <h5>Our vision</h5>
+                <p>
+                  To become a leading independent Company combining best
+                  practices, exceptional people and technical expertise
+                </p>
+              </div>
             </div>
             <div className="first col-sm-6">
-              <img src={ThirdIcon} alt="" /> <br />
-              <h5>Our mission</h5>
-              <p>
-                To consistently extract maximum value from desirable
-                opportunities and create maximum long term value for our
-                shareholders
-              </p>
+              <div>
+                <img src={ThirdIcon} alt="" /> <br />
+                <h5>Our mission</h5>
+                <p>
+                  To consistently extract maximum value from desirable
+                  opportunities and create maximum long term value for our
+                  shareholders
+                </p>
+              </div>
             </div>
             <div className="first col-sm-6">
-              <img src={ThirdIcon} alt="" />
-              <br />
-              <h5>Strategy</h5>
-              <p>
-                Our main strategy is to extract fossil fuel, produce water and
-                food in a sustainable matter with little or no negative impact
-                on the environment.
-              </p>
+              <div>
+                <img src={ThirdIcon} alt="" />
+                <br />
+                <h5>Strategy</h5>
+                <p>
+                  Our main strategy is to extract fossil fuel, produce water and
+                  food in a sustainable matter with little or no negative impact
+                  on the environment.
+                </p>
+              </div>
             </div>
           </div>
           <div className="core_values p-3 p-sm-5  rounded">
